1ff2d0b801246b7252641e935b8eb0c6853acfd9
zhangyue <soszhangyue@buaa.edu.cn>
2019-02-27 10:50:36 +0800

    add

---
+++ leaflet_2.php
@@ -0,0 +1,23 @@
+<?php
+$lat=$_POST["lat"];
+$lon=$_POST["lon"];
+switch ("$lat,$lon") {
+	case '-15.601,-47.713':
+		echo "<img src=\"leaflet_src/Brasilia\" alt=\"Brasilia\" />";
+		break;
+	case '60.1389,-1.1847':
+		echo "<img src=\"leaflet_src/Lerwick\" alt=\"Lerwick\" />";
+		break;
+	case '-89.983,-24.799':
+		echo "<img src=\"leaflet_src/South_Pole\" alt=\"South_Pole\" />";
+		break;
+	case '43.73,-96.62':
+		echo "<img src=\"leaflet_src/Sioux_Falls\" alt=\"Sioux_Falls\" />";
+		break;
+	case '22.7903,5.5292':
+		echo "<img src=\"leaflet_src/Tamanrasset\" alt=\"Tamanrasset\" />";
+		break;
+	default:
+		break;
+}
+?>
