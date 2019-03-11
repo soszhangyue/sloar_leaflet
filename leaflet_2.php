0502c915b834a7f9c89beeaf18b2babb4bbfbd5f
zhangyue <soszhangyue@buaa.edu.cn>
2019-03-11 19:36:09 +0800

    Yue

--- leaflet_2.php
+++ leaflet_2.php
@@ -1,29 +1,60 @@
 <?php
-$lat=$_POST["lat"];
-$lon=$_POST["lon"];
+/**
+ * 
+ */
+class Leaflet_Map_Spot
+{
+	public $lat;
+	public $lon;
+	public $Location;
+	public $error_log;
+	function __construct($latitude,$lontitude)
+	{
+		if (is_numeric($latitude)&&is_numeric($lontitude)&&$latitude>=-90&&$latitude<=90&&$lontitude>=-180&&$lontitude<=180) {
+			$this->lat=$latitude;
+			$this->lon=$lontitude;
+		}
+		else{
+			$this->error_log="Wrong Input";
+		}
+	}
+	function Inf_Match(){
+		switch ("$this->lat,$this->lon") {
+		case '-15.601,-47.713':
+			$this->Location='Brasilia';
+			break;
+		case '60.1389,-1.1847':
+			$this->Location='Lerwick';
+			break;
+		case '-89.983,-24.799':
+			$this->Location='South_Pole';
+			break;
+		case '43.73,-96.62':
+			$this->Location='Sioux_Falls';
+			break;
+		case '22.7903,5.5292':
+			$this->Location='Tamanrasset';
+			break;
+		default:
+			$this->error_log="Can't Match Any Spot In Database";
+			break;
+		}
+	}
+	function Weboutput(){
+		if (isset($this->error_log)) {
+			echo "<script>alert(\"$this->error_log\");</script>";
+		}
+		else{
+			echo "<img src=\"leaflet_src/$this->Location\" alt=\"$this->Location\" />";
+		}
+	}
+}
 if (isset($_POST["lat"])&&isset($_POST["lon"])) {
 	$lat=$_POST["lat"];
 	$lon=$_POST["lon"];
-	switch ("$lat,$lon") {
-		case '-15.601,-47.713':
-			echo "<img src=\"leaflet_src/Brasilia\" alt=\"Brasilia\" />";
-			break;
-		case '60.1389,-1.1847':
-			echo "<img src=\"leaflet_src/Lerwick\" alt=\"Lerwick\" />";
-			break;
-		case '-89.983,-24.799':
-			echo "<img src=\"leaflet_src/South_Pole\" alt=\"South_Pole\" />";
-			break;
-		case '43.73,-96.62':
-			echo "<img src=\"leaflet_src/Sioux_Falls\" alt=\"Sioux_Falls\" />";
-			break;
-		case '22.7903,5.5292':
-			echo "<img src=\"leaflet_src/Tamanrasset\" alt=\"Tamanrasset\" />";
-			break;
-		default:
-			echo "<script>alert(\"somthing wrong \");</script>";
-			break;
-	}
+	$Map_Spot= new Leaflet_Map_Spot($lat,$lon);
+	$Map_Spot->Inf_Match();
+	$Map_Spot->Weboutput();
 }
 else{
 	echo "<script>alert(\"net connection error \");</script>";
